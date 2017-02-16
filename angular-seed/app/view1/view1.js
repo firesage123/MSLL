fcfb985e5156d12fc9ec1080b4e7e9c4e31bf928
James He <he.james05@gmail.com>
2017-02-16 01:20:18 -0600

    P6 finished

--- angular-seed/app/view1/view1.js
+++ angular-seed/app/view1/view1.js
@@ -91,6 +91,19 @@ window.onload = init;
 function init() {
   var hello = document.getElementById('popupContainer').children[0].children[1];
   console.log(hello);
-  var goodbye = document.querySelector('#likes');
-  console.log(goodbye);
+}
+
+
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-red";
 }
